1b5ded1daa844d1e0c7199f602bd002b9731189e
Pedro Santana <pedroschw.c@gmail.com>
2023-08-01 11:45:07 -0300

    feat: created form component

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,12 @@
+
+ const Form = () => {
+    return (
+        <form>
+        <input type="text" placeholder="Enter participant names"/>
+
+        <button disabled={true} >Adicionar!</button>
+        </form>
+    )
+}
+
+export default Form
