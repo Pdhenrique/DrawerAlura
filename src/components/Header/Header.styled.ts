195986cab889652324b8f3a5a33208e1accf6993
Pedro Santana <pedroschw.c@gmail.com>
2023-08-01 11:44:46 -0300

    feat: created header component

---
+++ src/components/Header/Header.styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+import {colors} from '../../tokens/tokens'
+
+export const StyledHeader = styled.header`
+    height: 454px;
+    width: 1024px;
+
+    background-color: ${colors['--blue']},
+;
+`
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import {StyledHeader} from './Header.styled'
+
+
+export const Header = () => { 
+
+    return(
+        <StyledHeader>
+            <img alt="Logo"/>
+        </StyledHeader>
+    )
+}
+
+export default Header
